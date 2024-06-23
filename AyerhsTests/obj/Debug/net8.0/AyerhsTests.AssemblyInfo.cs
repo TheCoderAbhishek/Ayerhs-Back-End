0351a0f4dac34ebc7a7d0bf78caeb602791b275f
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-06-23 10:24:35 +0530

    add email credentials

--- AyerhsTests/obj/Debug/net8.0/AyerhsTests.AssemblyInfo.cs
+++ AyerhsTests/obj/Debug/net8.0/AyerhsTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AyerhsTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ec9c6d8d8a8c6ba59f24420fb83738ed4907cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c3085a1037eb399ec785919e044f268ea00d8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AyerhsTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AyerhsTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
